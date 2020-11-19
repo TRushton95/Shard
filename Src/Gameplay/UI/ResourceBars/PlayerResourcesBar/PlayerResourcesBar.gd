93daf696fef9965c1542fe7eb0828db27c3a8fe5
Tom Rushton <tom.rushton@hotmail.co.uk>
2020-11-19 23:11:58 +0000

    Add labels to player resources bar

--- Src/Gameplay/UI/ResourceBars/PlayerResourcesBar/PlayerResourcesBar.gd
+++ Src/Gameplay/UI/ResourceBars/PlayerResourcesBar/PlayerResourcesBar.gd
@@ -3,18 +3,22 @@ extends TextureRect
 
 func set_max_health(max_health: int) -> void:
 	$Health.max_value = max_health
+	_set_health_label($Health.value, max_health)
 
 
 func set_max_mana(max_mana: int) -> void:
-	$Health.max_value = max_mana
+	$Mana.max_value = max_mana
+	_set_mana_label($Mana.value, max_mana)
 
 
 func set_current_health(health: int) -> void:
 	$Health.value = health
+	_set_health_label(health, $Health.max_value)
 
 
 func set_current_mana(mana: int) -> void:
 	$Mana.value = mana
+	_set_mana_label(mana, $Mana.max_value)
 
 
 func initialise(max_health: int, max_mana: int) -> void:
@@ -22,3 +26,14 @@ func initialise(max_health: int, max_mana: int) -> void:
 	$Health.value = max_health
 	$Mana.max_value = max_mana
 	$Mana.value = max_mana
+	
+	_set_health_label(max_health, max_health)
+	_set_mana_label(max_mana, max_mana)
+
+
+func _set_health_label(value: int, max_value: int) -> void:
+	$Health/Label.text = str(value) + " / " + str(max_value)
+	
+	
+func _set_mana_label(value: int, max_value: int) -> void:
+	$Mana/Label.text = str(value) + " / " + str(max_value)
